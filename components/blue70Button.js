a6f221e252c765ae7323fe189943501c45b58d2c
blueseventy <petr@vabrousek.cz>
2025-10-12 23:51:17 +0200

    Add files via upload

--- components/blue70Button.js
+++ components/blue70Button.js
@@ -17,6 +17,7 @@ class Blue70Button extends Base {
                 },
                 radius: "1rem",
                 pad: [{lr: "2rem"}, {tb: "1rem"}],
+                mar: [{b: "1rem"}],
                 hide: this.obj.hide
               })
   }
